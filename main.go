9871d61fc7317b56c3e3f6c615b6573d5893a996
Stefan Bogdanovic <sbogdanovic@nanointeractive.com>
2024-01-12 11:38:08 +0100

    Github Actions fixed
    Signed-off-by: Stefan Bogdanovic <sbogdanovic@nanointeractive.com>

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+type Action struct {
+	Key   string
+	Value any
+	Type  string
+}
+
+type RedisMock struct {
+	data []Action
+}
+
 func main() {
 	fmt.Println("Start Here...")
 }
